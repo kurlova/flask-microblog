80608a4ad9a2eec69d0514473ca1635140f84e59
Yelena <sigizmoon@list.ru>
2017-06-24 00:08:25 +0500

    Implemented login with OpenID (lesson 3)

---
+++ config.py
@@ -0,0 +1,5 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'very_reliable_key'
+OPENID_PROVIDERS = [
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
--- microblog/views.py
+++ microblog/views.py
@@ -1,10 +1,13 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from microblog import app
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     # return "Hello, world!"
+
     user = {'nickname': 'Yelena'}
     posts = [
         {
@@ -21,3 +24,17 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="{0}", remember_me={1}'.format(form.openid.data,
+                                                                         str(form.remember_me.data)))
+
+        return redirect('/index')
+
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
